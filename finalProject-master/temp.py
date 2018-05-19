1590a4fa66d2c8f4dfc54030f9bb1f693e1cd4cf
Liron Gazit <lirongazit@Lirons-MacBook-Pro.local>
2018-05-19 14:12:24 +0300

    remove files2

--- finalProject-master/temp.py
+++
@@ -1,6 +0,0 @@
-
-for step in range(100):
-    nonrandom_batch, nonrandom_labels = get_next_nonrandom_batch(self.train_imgs_cropped, self.train_labels, self.img_size, self.batch_size)
-    _, session_classifier_loss = sess.run((self.optimizer2, tf.nn.sigmoid(self.classifier_loss)),
-                                          feed_dict={self.images: nonrandom_batch,self.tf_labels:nonrandom_labels,self.is_training:False})
-    print("step %d: classifier loss %f" % (step, np.mean(np.abs(nonrandom_labels-session_classifier_loss))))
